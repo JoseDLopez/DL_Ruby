f998c3ef7a29653d3124280e1537680c747dfea5
JoseDLopez <josed-lopez@hotmail.com>
2016-09-27 01:27:15 -0300

    Se agrega ejercicio 8

---
+++ Clase030_Ejercicios_08_group_Array.rb
@@ -0,0 +1,8 @@
+# Se tiene un ejercicio con una cantidad par de arreglos, se pide agruparlos de a dos.
+# ejemplo: [1,2,3,4,5,6,7,8] resultado: [[1,2],[3,4],[5,6],[7,8]]
+# hint: each_slice
+require 'pp'
+
+arreglo_par = [1,2,3,4,5,6,7,8,9,10]
+arreglo_agrupado = arreglo_par.each_slice(2)
+pp arreglo_par
