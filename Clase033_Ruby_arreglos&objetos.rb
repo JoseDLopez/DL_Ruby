bf42193dd6be02c6dc174413194530aea64ef55a
JoseDLopez <josed-lopez@hotmail.com>
2016-11-01 19:15:56 -0300

    Se actualiza el archivo de ejercicios de objetos de ruby con las soluciones de fracciones y barajas

--- Clase033_Ruby_arreglos&objetos.rb
+++ Clase033_Ruby_arreglos&objetos.rb
@@ -9,11 +9,10 @@ class Dice
 		@menor = menor
 		@mayor = mayor
 		puts "Se ha creado un dado con valores del #{menor} al #{mayor}"
-		@res = Random.new
 	end
 
 	def throw
-
+		@res = Random.new
 		@res = @res.rand(@menor..@mayor)
 	end
 end
@@ -21,6 +20,7 @@ end
 dado1 = Dice.new
 dado2 = Dice.new(1,10)
 pp dado2.throw
+pp dado1.throw
 
 puts "FINALIZA EL EJERCICIO 1"
 puts "_____________________________________________"
@@ -93,3 +93,137 @@ pedro = Alumno.new("Pedro")
 carlos = Alumno.new("Carlos",[10,12,16,25,01])
 puts jose.promedio
 puts carlos.promedio
+
+puts "FINALIZA EL EJERCICIO 3"
+puts "_____________________________________________"
+
+
+#EJERCICIO 4
+#FRACCIONES
+#Crear el objeto fracción, la fracción tiene dos parámetros, el numerador y el denominador.
+
+class Fraccion
+	def initialize (numerador=1,denominador=1)
+		@numerador = numerador
+		@denominador = denominador
+		puts "Se crea la fraccion #{@numerador}/#{@denominador}"
+	end
+
+	def numerador
+		@numerador
+	end
+
+	def denominador
+		@denominador
+	end
+  
+	def opera_con (segunda_fraccion,operacion='suma')
+		nuevo_denominador = @denominador * segunda_fraccion.denominador
+		if operacion == 'suma'
+			nuevo_numerador = (@denominador * segunda_fraccion.numerador) + (@numerador * segunda_fraccion.denominador)
+		elsif operacion =='resta'
+			nuevo_numerador = (@numerador * segunda_fraccion.denominador) - (@denominador * segunda_fraccion.numerador)
+		end
+
+		puts "El resultado de la #{operacion} de #{@numerador}/#{@denominador} con #{segunda_fraccion.numerador}/#{segunda_fraccion.denominador} es: #{nuevo_numerador}/#{nuevo_denominador}"
+
+	end
+
+end
+
+numero1 = Fraccion.new(4,5)
+numero2 = Fraccion.new(3,8)
+
+numero1.opera_con(numero2,'suma')
+numero1.opera_con(numero2,'resta')
+
+puts "FINALIZA EL EJERCICIO 4"
+puts "_____________________________________________"
+
+#EJERCICIO 5
+#BARAJA, MANO, CARTA
+#Construir la clase cata, debe guardar pinta y numero de carta.
+#Construir un arreglo con todas las cartas para conformar la baraja.
+#Construir clase Mano, esta contiene 5 cartas siempre.
+#La clase Mano, debe tener un metodo que reciba la baraja y coloque la primera carta de la mano al final de la baraja y la primera de la baraja al final de la mano. Despues del intercambio, barajear.
+
+
+class Carta
+	@@baraja=[]
+	def initialize (nombre,pinta)
+		@nombre = nombre
+		@pinta = pinta
+		@@baraja.push([@nombre,@pinta])
+	end
+
+	def self.baraja
+		@@baraja
+	end
+
+end
+
+class Mano
+	attr_accessor :mano
+	def initialize
+		@mano = []
+		for i in 1..5
+			rango = (Carta.baraja.length)-1
+			car = Random.new
+			car = car.rand(1..rango)
+			@mano.push(Carta.baraja[car])
+			Carta.baraja.slice!(car)
+		end
+	end
+
+	def recibe_baraja
+		Carta.baraja.push(self.mano.shift)
+		Carta.baraja.unshift(self.mano.pop)
+		puts "Veamos como queda la baraja con la primera y la ultima puesta"
+		pp Carta.baraja
+		puts "Ahora vamos a barajear"
+		pp Carta.baraja.shuffle
+	end
+
+end
+
+
+carta1 = Carta.new('1','Corazón Negro')
+carta2 = Carta.new('2','Corazón Negro')
+carta3 = Carta.new('3','Corazón Negro')
+carta4 = Carta.new('4','Corazón Negro')
+carta5 = Carta.new('5','Corazón Negro')
+carta6 = Carta.new('6','Corazón Negro')
+carta7 = Carta.new('7','Corazón Negro')
+carta8 = Carta.new('8','Corazón Negro')
+carta9 = Carta.new('9','Corazón Negro')
+
+carta11 = Carta.new('1','Corazón Rojo')
+carta12 = Carta.new('2','Corazón Rojo')
+carta13 = Carta.new('3','Corazón Rojo')
+carta14 = Carta.new('4','Corazón Rojo')
+carta15 = Carta.new('5','Corazón Rojo')
+carta16 = Carta.new('6','Corazón Rojo')
+carta17 = Carta.new('7','Corazón Rojo')
+carta18 = Carta.new('8','Corazón Rojo')
+carta19 = Carta.new('9','Corazón Rojo')
+
+carta21 = Carta.new('1','Trebol')
+carta22 = Carta.new('2','Trebol')
+carta23 = Carta.new('3','Trebol')
+carta24 = Carta.new('4','Trebol')
+carta25 = Carta.new('5','Trebol')
+carta26 = Carta.new('6','Trebol')
+carta27 = Carta.new('7','Trebol')
+carta28 = Carta.new('8','Trebol')
+carta29 = Carta.new('9','Trebol')
+
+pp Carta.baraja
+mano1 = Mano.new
+puts "Vamos a mostrar la mano"
+pp mano1.mano
+puts "Vamos a mostrar las cartas"
+pp Carta.baraja
+puts "Ahora vamos a recibir la baraja y la vamos a barajear"
+pp mano1.recibe_baraja
+puts "FINALIZA EL EJERCICIO 5"
+puts "_____________________________________________"
